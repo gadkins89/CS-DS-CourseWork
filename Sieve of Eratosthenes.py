1fbe3ed65c37899d881421f42d9879e2a96b6088
gadkins89 <113656910+gadkins89@users.noreply.github.com>
2023-02-13 22:20:09 -0600

    Sieve of Eratosthenes -- Finding Primes

---
+++ Sieve of Eratosthenes.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+from math import isqrt
+import time
+
+
+def find_primes (n: int):
+    if n <= 2:
+        return []
+    prime_num = [True] * n
+    prime_num[0] = False
+    prime_num[1] = False
+
+    for i in range(2, isqrt(n)):
+        if prime_num[i]:
+            for x in range(i*i, n, i):
+                prime_num[x] = False
+    return [i for i in range(n) if prime_num[i]]
+
+
+if __name__ == "__main__":
+
+    start = time.process_time()
+
+    y = int(input("Enter a number: \n"))
+
+    find_primes(y)
+
+    end = time.process_time() - start
+    
+
+    plt.plot(find_primes(y))
+    plt.title(f"Sieve of Eratosthenes Curve -- Solved In: {end} seconds. ")
+    plt.xlabel("Number of Primes ")
+    plt.ylabel(f"Given Number: {y} ")
+    plt.show()
